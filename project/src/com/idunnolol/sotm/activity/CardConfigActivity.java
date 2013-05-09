877cb66faeadde235fa0be40473609b16f0e42ac
Daniel Lew <dan@mobiata.com>
2013-05-09 17:41:54 -0500

    Made it easier to leave config activity
    It was pointed out to me that it's not obvious how to "save" your progress;
    now there's a "done" button which makes it obvious.  Also, up is enabled now
    as well.  It all does the same thing but gives the user choice.

--- project/src/com/idunnolol/sotm/activity/CardConfigActivity.java
+++ project/src/com/idunnolol/sotm/activity/CardConfigActivity.java
@@ -2,7 +2,10 @@ package com.idunnolol.sotm.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.idunnolol.sotm.R;
 import com.idunnolol.sotm.data.Db;
 import com.idunnolol.sotm.fragment.CardConfigListFragment;
 
@@ -24,6 +27,30 @@ public class CardConfigActivity extends Activity {
 		super.onPause();
 
 		Db.saveCardStates(this);
+	}
+
+	//////////////////////////////////////////////////////////////////////////
+	// Action bar
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_card_config, menu);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			finish();
+			return true;
+		case R.id.action_done:
+			finish();
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 
 }
