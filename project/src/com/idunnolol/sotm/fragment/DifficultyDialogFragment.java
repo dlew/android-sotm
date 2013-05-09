1781ef76306ffe120f9fd76def63e28a7f59eaca
Daniel Lew <dan@mobiata.com>
2013-05-09 15:45:37 -0500

    Renamed "impossible"
    Because technically it's not

--- project/src/com/idunnolol/sotm/fragment/DifficultyDialogFragment.java
+++ project/src/com/idunnolol/sotm/fragment/DifficultyDialogFragment.java
@@ -23,7 +23,7 @@ public class DifficultyDialogFragment extends DialogFragment {
 		MEDIUM(75, R.string.difficulty_medium),
 		HARD(50, R.string.difficulty_hard),
 		VERY_HARD(30, R.string.difficulty_very_hard),
-		IMPOSSIBLE(15, R.string.difficulty_impossible),
+		GOOD_LUCK(15, R.string.difficulty_good_luck),
 		PICK_YOUR_OWN(-1, R.string.difficulty_specify);
 
 		private int mTargetWinPercent;
