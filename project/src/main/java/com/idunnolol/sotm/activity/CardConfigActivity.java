54eb4abd393bb481cc4eb834b300f2ecafeea169
Dan Lew <danlew42@gmail.com>
2015-01-24 09:54:06 -0600

    Removed unnecessary "done" menu option

--- project/src/main/java/com/idunnolol/sotm/activity/CardConfigActivity.java
+++ project/src/main/java/com/idunnolol/sotm/activity/CardConfigActivity.java
@@ -2,9 +2,7 @@ package com.idunnolol.sotm.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
-import com.idunnolol.sotm.R;
 import com.idunnolol.sotm.data.Db;
 import com.idunnolol.sotm.fragment.CardConfigListFragment;
 
@@ -13,6 +11,8 @@ public class CardConfigActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
 
         if (savedInstanceState == null) {
             CardConfigListFragment fragment = new CardConfigListFragment();
@@ -32,19 +32,9 @@ public class CardConfigActivity extends Activity {
     // Action bar
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_card_config, menu);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                finish();
-                return true;
-            case R.id.action_done:
                 finish();
                 return true;
         }
